be1336c0eaf6e789b601f5af43c0146ba3b794e9
Daire O'Flaherty <daireoflaherty@PublicisD-Healthcare-Developer-1.local>
2013-11-20 14:20:23 +0000

    first commit

---
+++ SimpleApp/NewViewController.m
@@ -0,0 +1,38 @@
+//
+//  NewViewController.m
+//  SimpleApp
+//
+//  Created by Daire O'Flaherty on 04/10/2013.
+//  Copyright (c) 2013 Daire O'Flaherty. All rights reserved.
+//
+
+#import "NewViewController.h"
+
+@interface NewViewController ()
+
+@end
+
+@implementation NewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
